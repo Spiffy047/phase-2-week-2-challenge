5cfd8258ef6fa0dd1cd8bf0e15bac6ed175d368a
Joe M Kariuki <mwanikijoe1@gmail.com>
2025-08-09 14:02:01 +0300

    v30

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* global __firebase_config, __initial_auth_token, __app_id */
 import React, { useState, useEffect } from 'react';
 import { getAuth, signInAnonymously, onAuthStateChanged, signInWithCustomToken } from 'firebase/auth';
 import { getFirestore, collection, onSnapshot, doc, addDoc, setDoc, deleteDoc } from 'firebase/firestore';
